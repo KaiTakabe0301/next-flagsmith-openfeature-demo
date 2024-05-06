d04bdea7acd3dbfeb0d5ec7fa717eca20442a128
toefuVox <ph.c11.java.eo12@gmail.com>
2024-05-06 20:09:11 +0900

    chore: Update page.tsx with lazy loading for images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Typography } from "@/components/ui/typography";
+import { useFlags } from "flagsmith/react";
 
 export default function Home() {
+  const flags = useFlags(["show_demo_button"]);
   return (
     <div className="flex flex-col gap-4 justify-between items-center">
       <Typography variant="h1">Here&apos;s our button!</Typography>
-      <div id="submit_button">
-        <Button type="button">Flagsmith Quickstart Button!</Button>
-      </div>
+      {flags.show_demo_button.enabled && (
+        <div id="submit_button">
+          <Button type="button">Flagsmith Quickstart Button!</Button>
+        </div>
+      )}
     </div>
   );
 }
